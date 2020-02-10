f1516993ab9d28ab3291711527cb5cba598f164c
Patryk Oltuch <oltix@wp.pl>
2020-02-10 15:20:17 +0100

    Remove all code lines from JS, CSS, HTML

--- js/index.js
+++
@@ -1,7 +0,0 @@
-const hamburger = document.querySelector(".hamburger");
-const sideBar = document.querySelector(".navigation");
-
-hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("hamburger--active");
-    sideBar.classList.toggle("navigation--active");
-})
